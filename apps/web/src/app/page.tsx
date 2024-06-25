b26dce16753694a3d4e6ae72fb16f023e8cc6620
Av-code80 <dev.avehghasemian@gmail.com>
2024-06-25 21:13:49 +0200

    feat: :sparkles: Implement new features in page.tsx and types.ts file in src

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,18 +1,25 @@
-// apps/web/src/app/page.tsx
 "use client";
 
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { getWeather } from "@/actions/weather";
 import { usePersistData } from "@/components/use-keep-data";
+import TemperatureThresholdForm from "@/components/TemperatureThresholdForm";
+import WeatherTable from "@/components/WeatherTable";
+import { WeatherDataGroupResponse, WeatherData } from "@/common/types";
 
 const Home: React.FC = () => {
-  const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState<WeatherData[]>([]);
+  const [threshold, setThreshold] = useState<number>(0);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await getWeather();
-      setWeatherData(data);
+      try {
+        const data: WeatherDataGroupResponse = await getWeather();
+        setWeatherData(data.list);
+      } catch (error) {
+        console.error("Failed to fetch weather data:", error);
+      }
     };
 
     fetchData();
@@ -20,25 +27,23 @@ const Home: React.FC = () => {
 
   const data = usePersistData(weatherData);
 
-  console.log("weatherData : ", data);
 
-  return (
-    <div className="p-7 w-[100%] h-[100%]">
-      <Image
-        src="/logo.png"
-        width={500}
-        height={200}
-        alt="Tracklab logo"
-        className="m-auto mb-8"
-      />
-      <h1 className="text-2xl font-bold text-center">
-        Welcome to the test, you can start writing in this file 👇
-      </h1>
-      <h2 className="text-xl font-semibold text-center">
-        Good luck and do not hesitate if you have any questions to reach us
-      </h2>
-    </div>
-  );
+ return (
+   <div className="p-7 w-[100%] h-[100%]">
+     <Image
+       src="/logo.png"
+       width={100}
+       height={100}
+       alt="Tracklab logo"
+       className="m-auto mb-8"
+       priority={true}
+       style={{ width: "auto", height: "auto" }}
+     />
+
+     <TemperatureThresholdForm onThresholdChange={setThreshold} />
+     <WeatherTable weatherData={data || []} threshold={threshold} />
+   </div>
+ );
 };
 
 export default Home;
